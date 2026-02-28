cb840d180d44f8b2e5af82cf9d7d56827b556dc7
Borges <borgespaulo72@yahoo.com>
2026-02-28 13:59:13 -0300

    fix: refatorando logimForm

--- src/Components/Login/LoginForm.tsx
+++ src/Components/Login/LoginForm.tsx
@@ -1,61 +1,13 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
-const formFields = [
-  {
-    id: "login",
-    type: "text",
-    label: "Login",
-  },
-  {
-    id: "password",
-    type: "password",
-    label: "Password",
-  },
-];
-
 export const LoginForm = () => {
-  const [formData, setFormData] = useState({
-    login: "",
-    password: "",
-  });
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    localStorage.setItem("@glory:entrar", JSON.stringify(formData));
-    console.log("Estamos no Caminho certo", formData);
-  }
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  }
-
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <div>
+      <h2>Formulário de Membro</h2>
+      <form>
         <div>
-          {formFields.map(({ id, label, type }) => (
-            <div key={id}>
-              <label htmlFor={label}>{label}</label>
-              <input
-                name={id}
-                id={id}
-                type={type}
-                value={formData[id as keyof typeof formData]}
-                onChange={handleChange}
-              />
-            </div>
-          ))}
-
-          <button>Entrar</button>
+          <label>Nome:</label>
+          <input type="text" name="nome" placeholder="Digite o seu nome" />
         </div>
       </form>
-      <Link to="/Login/criar">Cadastro:</Link>
-    </section>
+    </div>
   );
 };
