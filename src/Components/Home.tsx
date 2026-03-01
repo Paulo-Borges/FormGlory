d35d8e0f9e948efb209f32bbde272ee6e69d6994
Borges <borgespaulo72@yahoo.com>
2026-03-01 15:46:55 -0300

    fix: colocando os input´s dinamicos

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
 
+const formFields = [
+  {
+    label: "Nome",
+    type: "text",
+    id: "nome",
+    placeholder: "Digite o seu nome",
+  },
+  {
+    label: "Email",
+    type: "email",
+    id: "email",
+    placeholder: "Digite o email cadastrado!",
+  },
+];
+
 export const Home = () => {
   const [data, setData] = useState({
     nome: "",
@@ -18,16 +33,19 @@ export const Home = () => {
   return (
     <div>
       <form onSubmit={enviarForm}>
-        <div>
-          <label htmlFor="">Nome:</label>
-          <input
-            type="text"
-            name="nome"
-            placeholder="Digite o seu nome"
-            onChange={pegarForm}
-          />
-        </div>
-        <div>
+        {formFields.map((field) => (
+          <div key={field.id}>
+            <label htmlFor="">{field.label}:</label>
+            <input
+              type={field.type}
+              name={field.id}
+              placeholder={field.placeholder}
+              onChange={pegarForm}
+            />
+          </div>
+        ))}
+
+        {/* <div>
           <label htmlFor="">Email:</label>
           <input
             type="email"
@@ -35,7 +53,7 @@ export const Home = () => {
             placeholder="Digite o email cadastrado"
             onChange={pegarForm}
           />
-        </div>
+        </div> */}
         <button type="submit">Entrar / Criar</button>
       </form>
     </div>
