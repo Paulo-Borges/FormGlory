56997be620fca73d31672b2f5759a7b26b54b3cf
Borges <borgespaulo72@yahoo.com>
2026-03-01 15:36:45 -0300

    Iniciando Home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 export const Home = () => {
+  const [data, setData] = useState({
+    nome: "",
+    email: "",
+  });
+
+  function pegarForm(e) {
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
+
+  function enviarForm(e) {
+    e.preventDefault();
+    console.log(data);
+  }
+
   return (
     <div>
-      <h1>Home</h1>
+      <form onSubmit={enviarForm}>
+        <div>
+          <label htmlFor="">Nome:</label>
+          <input
+            type="text"
+            name="nome"
+            placeholder="Digite o seu nome"
+            onChange={pegarForm}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Email:</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o email cadastrado"
+            onChange={pegarForm}
+          />
+        </div>
+        <button type="submit">Entrar / Criar</button>
+      </form>
     </div>
   );
 };
